8a721b0668c690eb34a822f6bb314f9b4989b7ed
xenturion111 <rezaaditiyam@gmail.com>
2022-04-11 20:21:10 +0700

    Commit pertama

---
+++ src/components/atoms/button/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './button.css';
+import Button from '@mui/material/Button';
+
+const Button = () => {
+    <div>
+        <Button id='button-search' variant="contained">Cari</Button>
+    </div>
+}
+
+export default Button;
---
+++ src/components/molecules/navbar/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Button from '../../atoms/button/index'
+import Search from "../../atoms/search/index"
+
+
+const Navbar = () => {
+    
+}
