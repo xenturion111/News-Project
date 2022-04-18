8c91fb6a461b1fc32f286e1fed896359b2e01543
xenturion111 <rezaaditiyam@gmail.com>
2022-04-18 16:21:32 +0700

    berhasil di commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,27 @@ import React from 'react'
 import './App.css'
 import Navbar from './components/navBar/index'
 
-
 function App() {
   return (
     <div className="App">
       <section className="Header">
         <Navbar />
+          <div class="Iam">
+            <p>New-News</p>
+            <b>
+              <div class="innerIam">
+                Tempat<br /> 
+                Berita terkini<br />
+                Berita terpercaya<br />
+                Berita Terbaik<br />
+                Hanya di NEW NEWS
+                </div>
+            </b>
+          </div>
       </section>
-
+      <section>
+        
+      </section>
     </div>
   )
 }
