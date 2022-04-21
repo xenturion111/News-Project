cf2c9d7e97129e0b805379ca2b01624dc5894531
xenturion111 <rezaaditiyam@gmail.com>
2022-04-21 13:18:08 +0700

    menambahkan button

--- src/NewsContext.jsx
+++ src/NewsContext.jsx
@@ -7,6 +7,10 @@ export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
   const apiKey = "7ac509953f2b474181234c50d273d957";
 
+
+  // const request1 = axios.get(`https://newsapi.org/v2/top-headlines?country=id&apiKey=${apiKey}`);
+  // const request2 =  axios.get(`https://newsapi.org/v2/everything?q=apple&from=2022-04-20&to=2022-04-20&sortBy=popularity&apiKey=${apiKey}`)
+  
   useEffect(() => {
     axios
       .get(
